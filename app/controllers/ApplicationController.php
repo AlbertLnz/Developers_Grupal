3a229ee9e794bf3e57520d27936be40aa5c3e27e
Albert Lanza Rio <albert2000.lanza@gmail.com>
2023-04-26 20:39:47 +0200

    Fix ApplicationController creating a construct instead of multiple calls to Model

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -7,8 +7,14 @@
 class ApplicationController extends Controller 
 {
 	
+  private $persistenciaJson;
+
+  public function __construct(){
+    $this->persistenciaJson = new jsonPersistance();
+  }
+
 	public function indexAction()
 	{
-		$this->view->allTasks = new jsonPersistance();
+		$this->view->allTasks = $this->persistenciaJson->getAllTasks();
 	}
 }
